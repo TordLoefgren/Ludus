bc940a1cb4799ae82cd7478f7c241e7aeee009ef
Tord <tord0505@hotmail.com>
2025-10-10 23:22:55 +0200

    feat: Added Google test project with sample tests.

---
+++ Ludus.Tests/Math/Bounds2DTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+
+namespace LudusTests::Math
+{
+	using Ludus::Math::Bounds2D;
+
+	TEST(Bounds2D, PLACEHOLDER)
+	{
+		// Arrange.
+
+		// Act.
+
+		// Assert.
+		EXPECT_TRUE(true);
+	}
+}
---
+++ Ludus.Tests/pch.h
@@ -0,0 +1,10 @@
+//
+// pch.h
+//
+
+#pragma once
+
+#include "gtest/gtest.h"
+
+#include <Ludus/Math/Vector2D.h>
+#include <Ludus/Math/Bounds2D.h>
