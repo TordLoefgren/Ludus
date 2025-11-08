9f665e0e5f401f19a75d05c44e41c4eb54af2fc2
Tord <tord0505@hotmail.com>
2025-11-08 13:51:54 +0100

    fix: Added ignored file to source control.

--- Ludus.Lab/main.cpp
+++ Ludus.Lab/main.cpp
@@ -17,9 +17,6 @@ const int HEIGHT = 640;
 
 int main()
 {
-	//auto application = Ludus::Core::Application::Create();
-	//application->Run();
-
 	auto windowOptions = Ludus::Platform::WindowOptions(WIDTH, HEIGHT, "Ludus Lab", false);
 	auto eventBus = Ludus::Events::EventBus();
 	auto input = Ludus::Platform::Input();
