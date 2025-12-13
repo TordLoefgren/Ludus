b05753b622dc6423174b86c96ffe31c0383c7932
Tord <tord0505@hotmail.com>
2025-12-13 01:11:27 +0100

    feat: Added aggregate console text.

--- Editor/Source/Ludus/Editor/Panels/ConsolePanel.cpp
+++ Editor/Source/Ludus/Editor/Panels/ConsolePanel.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 
+#include <format>
+
 #include <Ludus/Editor/Panels/ConsolePanel.h>
 
 namespace Ludus::Editor::Panels
@@ -10,14 +12,41 @@ namespace Ludus::Editor::Panels
 		if (Ludus::UI::Containers::Window window("Console", &open, Constants::PanelFlags); window)
 		{
 			auto& logEntries = Ludus::Engine::Debug::GetLogEntries();
+
 			for (auto& entry : logEntries)
 			{
-				Ludus::UI::Widgets::Text(FormatEntry(entry));
+				auto formattetText = FormatEntry(entry);
+
+				auto iter = m_TextToIndex.find(formattetText);
+				if (iter == m_TextToIndex.end())
+				{
+					auto size = m_AggregateText.size();;
+					m_AggregateText.push_back({ std::move(formattetText), 1 });
+					m_TextToIndex[m_AggregateText.back().Text] = size;
+				}
+				else
+				{
+					m_AggregateText.at(iter->second).Count++;
+				}
 			}
+
+			for (auto& text : m_AggregateText)
+			{
+				if (text.Count == 1)
+				{
+					Ludus::UI::Widgets::Text(text.Text);
+				}
+				else
+				{
+					Ludus::UI::Widgets::Text(std::format("{} ({})", text.Text, text.Count));
+				}
+			}
+
+			logEntries.clear();
 		}
 	}
 
-	std::string ConsolePanel::FormatEntry(Ludus::Engine::Debug::LogEntry& entry)
+	std::string ConsolePanel::FormatEntry(const Ludus::Engine::Debug::LogEntry& entry)
 	{
 		if (entry.Tag.empty())
 		{
